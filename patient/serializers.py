5835b0b545edaebd1370c37ead7aa6b48d5f01b5
zcq <2723026135@qq.com>
2024-06-25 15:22:12 +0800

    patient serializer未保存

--- patient/serializers.py
+++ patient/serializers.py
@@ -6,13 +6,6 @@ class PatientSerializer(serializers.ModelSerializer):
         model = Patient
         fields = '__all__'
 
-class PatientCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Patient
-        fields = ('name','age','region','address','phone','id_card','gender','medical_history','allergy_history')
-        # 设置名称为非必须
-        extra_kwargs = {'name': {'required': False}}
-        extra_kwargs = {'age': {'required': False}}
 
 
 
